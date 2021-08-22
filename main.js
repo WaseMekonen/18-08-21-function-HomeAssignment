0ea025631e704a231e78038132e20a1d6ba4ccec
MekonenWase <mekonen.wase@gmail.com>
2021-08-22 19:41:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,325 @@
+// Exercise 1://
+
+// function isStrLong(string){
+//     if(string.length>5){
+//         return "long";
+//     }
+//     return "short"
+// }
+
+// Exercise 2://
+
+// function isCharOnStr(string){
+//     if(string.indexOf('a')>0){
+//         return console.log(string.indexOf('a'));
+//     }
+//     return console.log("not found!");
+// }
+
+// Exercise 3://
+
+// function getIndexOfChar(string){
+//     if(string.indexOf('r')>-1){
+//         return console.log(true);
+//     }
+//     return console.log(false);
+// }
+
+// Exercise 4://
+
+// function isStringContainChar(string,chr){
+//     if(string.indexOf(chr)>-1){
+//         return console.log(true);
+//     }
+//     return console.log(false);
+
+// }
+
+// Exercise 5://
+
+// function isCharOnString(str,char){
+//     if(str.lastIndexOf(char)==-1){
+//        return "there is no string";
+//     }
+//     return str.lastIndexOf(char);
+// }
+
+// Exercise 6://
+
+// function getThreeFirstWord(string){
+//     console.log(string.substr(0,3));
+// }
+
+// Exercise 7://
+
+// function getFourWordFromSomeIndex(string){
+//     var y=string.indexOf('y');
+//     return (string.substr(y,4));
+// }
+
+// Exercise 8://
+
+// function getAllStringFromSomeChar(string){
+//     var o=string.indexOf('o');
+//     return (string.substr(o,string.length-1));
+// }
+
+// Exercise 9://
+
+// function isStrExicsOnString(string,char){
+//     if(char>string.length){
+//         return ("the char is bigger than string");
+//     }
+//     else if(char<=string.length){
+
+//         return (string.substr(char,string.length));
+//     }
+// }
+
+// Exercise 10://
+
+// function isStrExicsOnString(string,char){
+//     if(char>string.length){
+//         return ("the Char is too Big");
+//     }
+//     else if(char<=string.length){
+//         return (string.substr(char,3));
+//     }
+// }
+
+// Exercise 11://
+
+// function getSomeOfString(num1,num2,string){
+//     if(num1>0 && num2>0){
+//        return (string.substr(num1,num2));
+//     }
+
+// }
+
+// Exercise 12://
+//  A:
+
+// function getFirstThreeChar(string){
+//     return(string.substring(0,3));
+
+// }
+
+// B:
+
+// function getFourCharfromY(string){
+//     var y=string.indexOf("y");
+//     return string.substring(y,y+4);
+// }
+
+// c:
+
+// function getAllCharfromO(string){
+//     var o=string.indexOf("o");
+//     console.log(string.substring(o));
+// }
+
+// getAllCharfromO("sayonara");
+
+
+// Exercise 15://
+// A:
+
+// function getSumOFchar(string){
+//     console.log( string.length);
+// }
+
+// B:
+
+// function isSpaceOnStr(string){
+//     if(string.indexOf(" ")!=-1){
+//         console.log();
+//     };
+// }
+
+// C:
+
+// function getFnameAndLname(fullName){
+//     var space= fullName.indexOf(" ");
+//     var fName = fullName.substring(0,space);
+//     var lName = fullName.substring(space+1);
+//     return `firstName:${fName} lastName:${lName}`;
+// }
+
+// Exercise 16://
+
+// function isStringEqualByLowerCase(string1,string2){
+//     var newStr1 =string1.toLowerCase();
+//     var newStr2 =string2.toLowerCase();
+//     if(newStr1 == newStr2){
+//         return "string are equal"
+//     }
+//     return "string are not equal!"
+// }
+
+
+// Exercise 17://
+
+
+// function isStringEqualByUpperCase(string1,string2){
+//     var newStr1 =string1.toUpperCase();
+//     var newStr2 =string2.toUpperCase();
+//     if(newStr1 == newStr2){
+//         return "string are equal"
+//     }
+//     return "string are not equal!"
+// }
+
+// Exercise 18://
+
+// function getLongestName(fName,lName){
+//     if(fName.length>lName.length){
+//         return (fName);
+//     }
+//     else if(lName.length>fName.length){
+//          return (lName);
+//     }
+//     else if(lName.length == fName.length)
+//     return "both of them are equal!"
+// }
+
+// Exercise 19://
+
+// function isSomeCharInNames(fName,lName){
+//     if(fName.indexOf('a')!=-1){
+//         "char is in first name"
+//     }else{
+//         "char is not in first name"
+//     }
+//     if(lName.indexOf('api')!=-1){
+//         "char is in last name"
+//     }else{
+//         return "char is not in last name"
+//     }
+// }
+
+// Exercise 20://
+
+// function isStringLong(string){
+//     if(string.length>=5){
+//         return "long"
+//     }
+//     return "short"
+// }
+
+// Exercise 21://
+
+// function isCountryLenShortThanTree(country){
+//     if(country.length <=3){
+//         return "yes"
+//     }
+//     return "no"
+// }
+
+// Exercise 22://
+
+// function getStringOrIndexOfChar(string,char){
+//     if(string.length>6){
+//         return string;
+//     }
+//     else if(string.indexOf(char)==-1){
+//         return string.indexOf(char);
+//     }
+//     return string.indexOf(char);
+
+// }
+
+// Exercise 23://
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
